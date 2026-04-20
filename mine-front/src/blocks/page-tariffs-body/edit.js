ad7bfd8d8b231232092aa84fab9243c4e421124e
SochnayaHurma <121946362+SochnayaHurma@users.noreply.github.com>
2026-04-20 15:46:29 +0300

    Enrich reporting and tariffs body templates

--- mine-front/src/blocks/page-tariffs-body/edit.js
+++ mine-front/src/blocks/page-tariffs-body/edit.js
@@ -4,13 +4,25 @@ const TEMPLATE = [
 	[ 'slav-bank/kicker', { text: 'Тарифы' } ],
 	[ 'slav-bank/title', { title: 'Тарифы банка', level: '2' } ],
 	[
-		'slav-bank/page-pattern-list',
+		'core/paragraph',
 		{
-			items: [
-				{ id: 'trf-1', text: 'Тарифы по операциям в валюте РФ.' },
-				{ id: 'trf-2', text: 'Тарифы по операциям в иностранной валюте.' },
-				{ id: 'trf-3', text: 'Специальные тарифные планы «Приветственный» и «Славный».' },
-			],
+			content:
+				'Выберите нужный раздел тарифов. Каждый раздел поддерживает редактирование карточек документов и ссылок в Gutenberg.',
+		},
+	],
+	[
+		'core/list',
+		{
+			values:
+				'<ul><li><a href="/tariffs-rub/">Тарифы по операциям в валюте РФ</a></li><li><a href="/tariffs-foreign-currency/">Тарифы по операциям в иностранной валюте</a></li><li><a href="/tariff-privetstvenny/">Тариф «Приветственный»</a></li><li><a href="/tariffs-slavny/">Тариф «Славный»</a></li><li><a href="/tariff-depositny/">Тариф «Депозитный»</a></li></ul>',
+		},
+	],
+	[
+		'core/paragraph',
+		{
+			content:
+				'Перед публикацией проверяйте соответствие тарифных документов действующим условиям банка и внутренним регламентам.',
+			className: 'muted',
 		},
 	],
 ];
